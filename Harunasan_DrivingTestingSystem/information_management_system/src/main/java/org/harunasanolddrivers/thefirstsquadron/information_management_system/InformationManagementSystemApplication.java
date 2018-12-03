6db4a6e79a3f742f8f094ab46646f179434fb864
HTLAX123456 <herli@deloitte.com.cn>
2018-12-03 13:39:03 +0800

    项目搭建

---
+++ Harunasan_DrivingTestingSystem/information_management_system/src/main/java/org/harunasanolddrivers/thefirstsquadron/information_management_system/InformationManagementSystemApplication.java
@@ -0,0 +1,12 @@
+package org.harunasanolddrivers.thefirstsquadron.information_management_system;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InformationManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InformationManagementSystemApplication.class, args);
+    }
+}

8a466113f64023902d119b1b9c875e48c38f379e
啃食树根的恶龙 <HTLAX123456@gmail.com>
2019-02-14 18:56:20 +0800

    结构整理
    添加了mapper和dao

---
+++ HarunasanDrivingTestingSystem/src/main/java/com/cqnu/harunasandrivingtestingsystem/dao/UserTableMapper.java
@@ -0,0 +1,17 @@
+package com.cqnu.harunasandrivingtestingsystem.dao;
+
+import com.cqnu.harunasandrivingtestingsystem.entity.UserTable;
+
+public interface UserTableMapper {
+    int deleteByPrimaryKey(Integer userId);
+
+    int insert(UserTable record);
+
+    int insertSelective(UserTable record);
+
+    UserTable selectByPrimaryKey(Integer userId);
+
+    int updateByPrimaryKeySelective(UserTable record);
+
+    int updateByPrimaryKey(UserTable record);
+}

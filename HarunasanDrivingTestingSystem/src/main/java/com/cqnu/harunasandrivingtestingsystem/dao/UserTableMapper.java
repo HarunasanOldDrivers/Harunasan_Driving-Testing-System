c06fbb5698c5a75e16da88de1df2ed18d74c6b0d
bigge <582364398@qq.com>
2019-02-20 14:55:51 +0800

    1

--- HarunasanDrivingTestingSystem/src/main/java/com/cqnu/harunasandrivingtestingsystem/dao/UserTableMapper.java
+++ HarunasanDrivingTestingSystem/src/main/java/com/cqnu/harunasandrivingtestingsystem/dao/UserTableMapper.java
@@ -12,6 +12,6 @@ public interface UserTableMapper {
     UserTable selectByPrimaryKey(Integer userId);
 
     int updateByPrimaryKeySelective(UserTable record);
-//==
+//==！
     int updateByPrimaryKey(UserTable record);
 }

6440c95b492c1f8cdffbdee5179d9572256fe4b2
啃食树根的恶龙 <HTLAX123456@gmail.com>
2019-03-20 22:30:19 +0800

    普通用户基础功能

--- HarunasanDrivingTestingSystem/src/main/java/com/cqnu/harunasandrivingtestingsystem/security/WebSecurityConfig.java
+++ HarunasanDrivingTestingSystem/src/main/java/com/cqnu/harunasandrivingtestingsystem/security/WebSecurityConfig.java
@@ -1,15 +1,59 @@
 package com.cqnu.harunasandrivingtestingsystem.security;
 
+import com.cqnu.harunasandrivingtestingsystem.security.filter.AuthenticationTokenFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 /**
  * @author LiAixing
  * @version 1.0
  * @className WebSecurityConfig
  * @description TODO
- * @date 2019/2/23 18:07
+ * @date 2019/3/20 11:11
  **/
-@EnableGlobalMethodSecurity
+@Configuration
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public AuthenticationTokenFilter authenticationTokenFilterBean() throws Exception {
+        AuthenticationTokenFilter authenticationTokenFilter = new AuthenticationTokenFilter();
+        authenticationTokenFilter.setAuthenticationManager(authenticationManagerBean());
+        return authenticationTokenFilter;
+    }
+
+
+
+    @Override
+    protected void configure(HttpSecurity httpSecurity) throws Exception {
+
+
+        httpSecurity.csrf().disable().
+                sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+                .and()
+                .authorizeRequests()
+                .antMatchers("/**").permitAll()
+                .antMatchers("/druid/**").permitAll()
+                .antMatchers("/api/**").permitAll()
+                // 把不需要认证的接口暴露出去。登录，刷新token，
+                .antMatchers("/auth/**").permitAll()
+                .anyRequest().authenticated()
+                .and().headers().cacheControl();
+        httpSecurity
+                .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class);
+
+        // 这块是配置跨域请求的
+        // ExpressionUrlAuthorizationConfigurer<HttpSecurity>.ExpressionInterceptUrlRegistry registry = httpSecurity.authorizeRequests();
+        //让Spring security放行所有preflight request
+//        registry.requestMatchers(CorsUtils::isPreFlightRequest).permitAll();
+    }
+
+
 }

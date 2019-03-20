3c709af247a3d45af4e7bb34a38369a14ec67d2c
啃食树根的恶龙 <HTLAX123456@gmail.com>
2019-03-20 12:33:49 +0800

    添加jwt

---
+++ HarunasanDrivingTestingSystem/src/main/java/com/cqnu/harunasandrivingtestingsystem/entity/QuestionsFour.java
@@ -0,0 +1,146 @@
+package com.cqnu.harunasandrivingtestingsystem.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+public class QuestionsFour extends Questions{
+    private Integer qoId;
+
+    private String qoType;
+
+    private String qoTitle;
+
+    private String qoOptionA;
+
+    private String qoOptionB;
+
+    private String qoOptionC;
+
+    private String qoOptionD;
+
+    @JsonIgnore
+    private String qoAnswer;
+
+    private String qoVideo;
+
+    private String qoImage;
+
+    private String qoDescription;
+
+    private Integer qoDifficulty;
+
+    private String qoKnowledge;
+
+    private String qoChapter;
+
+    public Integer getQoId() {
+        return qoId;
+    }
+
+    public void setQoId(Integer qoId) {
+        this.qoId = qoId;
+    }
+
+    public String getQoType() {
+        return qoType;
+    }
+
+    public void setQoType(String qoType) {
+        this.qoType = qoType == null ? null : qoType.trim();
+    }
+
+    public String getQoTitle() {
+        return qoTitle;
+    }
+
+    public void setQoTitle(String qoTitle) {
+        this.qoTitle = qoTitle == null ? null : qoTitle.trim();
+    }
+
+    public String getQoOptionA() {
+        return qoOptionA;
+    }
+
+    public void setQoOptionA(String qoOptionA) {
+        this.qoOptionA = qoOptionA == null ? null : qoOptionA.trim();
+    }
+
+    public String getQoOptionB() {
+        return qoOptionB;
+    }
+
+    public void setQoOptionB(String qoOptionB) {
+        this.qoOptionB = qoOptionB == null ? null : qoOptionB.trim();
+    }
+
+    public String getQoOptionC() {
+        return qoOptionC;
+    }
+
+    public void setQoOptionC(String qoOptionC) {
+        this.qoOptionC = qoOptionC == null ? null : qoOptionC.trim();
+    }
+
+    public String getQoOptionD() {
+        return qoOptionD;
+    }
+
+    public void setQoOptionD(String qoOptionD) {
+        this.qoOptionD = qoOptionD == null ? null : qoOptionD.trim();
+    }
+
+    public String getQoAnswer() {
+        return qoAnswer;
+    }
+
+    public void setQoAnswer(String qoAnswer) {
+        this.qoAnswer = qoAnswer == null ? null : qoAnswer.trim();
+    }
+
+    public String getQoVideo() {
+        return qoVideo;
+    }
+
+    public void setQoVideo(String qoVideo) {
+        this.qoVideo = qoVideo == null ? null : qoVideo.trim();
+    }
+
+    public String getQoImage() {
+        return qoImage;
+    }
+
+    public void setQoImage(String qoImage) {
+        this.qoImage = qoImage == null ? null : qoImage.trim();
+    }
+
+    public String getQoDescription() {
+        return qoDescription;
+    }
+
+    public void setQoDescription(String qoDescription) {
+        this.qoDescription = qoDescription == null ? null : qoDescription.trim();
+    }
+
+    public Integer getQoDifficultty() {
+        return qoDifficulty;
+    }
+
+    public void setQoDifficultty(Integer qoDifficultty) {
+        this.qoDifficulty = qoDifficultty;
+    }
+
+    public String getQoKnowledge() {
+        return qoKnowledge;
+    }
+
+    public void setQoKnowledge(String qoKnowledge) {
+        this.qoKnowledge = qoKnowledge == null ? null : qoKnowledge.trim();
+    }
+
+    public String getQoChapter() {
+        return qoChapter;
+    }
+
+    public void setQoChapter(String qoChapter) {
+        this.qoChapter = qoChapter == null ? null : qoChapter.trim();
+    }
+}
